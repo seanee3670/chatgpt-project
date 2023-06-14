f49f2e65ab70dca1bff1c928332702ceb8a2795e
seanee3670 <cseok3670@gmail.com>
2023-06-14 17:24:07 +0900

    [init] add test(empty)

---
+++ src/test/java/com/shaun/chatgptproject/ChatgptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.chatgptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
