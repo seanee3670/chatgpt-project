f125c7743718393a6b0033dd76a05caf42ca78d9
seanee3670 <cseok3670@gmail.com>
2023-06-14 17:20:31 +0900

    [init] add App

---
+++ src/main/java/com/shaun/chatgptproject/ChatgptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shaun.chatgptproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptProjectApplication.class, args);
+	}
+
+}
